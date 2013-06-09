b3d03cae148d858e6cc75b0f1b5bf2da8c6ae021
Albert Bellonch Llargués <albert@itnig.net>
2013-06-09 23:53:10 +0200

    Implement tests for the behaviour

--- spec/integration/okapi.rb
+++ spec/integration/okapi.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Okapi do
   class TestClass
     attr_accessor :username
+    attr_hider :password
 
     def initialize(username, password)
       @username = username
@@ -10,7 +11,16 @@ describe Okapi do
     end
   end
 
-  it "should show the name and value of the username"
+  before do
+    @instance = TestClass.new('myusername', 'mypassword')
+    @inspect = @instance.inspect
+  end
 
-  it "should not show the name neither the value of the username"
+  it "should show the name and value of the username" do
+    @inspect.should =~ /@username=\"myusername\"/
+  end
+
+  it "should not show the name neither the value of the username" do
+    @inspect.should_not =~ /@password=\"mypassword\"/
+  end
 end
